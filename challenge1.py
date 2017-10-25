4c2b1f9c01bebe569753636fca01b37b46b005e1
Soren Holm <jhssoren@hotmail.com>
2017-10-24 21:04:17 -0400

    Update challenge1.py
    Challenge 1 complete

--- challenge1.py
+++ challenge1.py
@@ -5,6 +5,9 @@ outputFile = open("CfloridaCounts.txt","w")
 
 regexTX = re.compile(r"(CF|cf)\w*.[Aa]\w*.")
 regexFL = re.compile(r"(CF|cf).[Gg]\w*.")
+delete1 = re.compile("0/0:")
+delete2 = re.compile(":\d[\d:,]*")
+delete3 = re.compile("./.:.:.:.:.")
 
 for ln in inputFile:
     ln.strip()
@@ -16,6 +19,9 @@ for ln in inputFile:
         ln = re.sub(regexTX,"Cf.Sfa.",ln)
         outputFile.write(ln + "\n")
     else:
+        ln = re.sub(delete1, "", ln)
+        ln = re.sub(delete2, "", ln)
+        ln = re.sub(delete3, "NA", ln)
         outputFile.write(ln + "\n")
         
 inputFile.close()
