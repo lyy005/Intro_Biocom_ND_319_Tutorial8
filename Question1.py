b72bb2396a87b53fb53843eef00492e5891c226e
ayamasaki2011 <ayamasak@nd.edu>
2017-10-26 15:39:40 -0400

    AEY: removed unnecessary code to check for carats, and wrote header line to file - having trouble with replacing and regex

--- Question1.py
+++ Question1.py
@@ -5,33 +5,23 @@ vcffile=open("Cflorida.vcf","r")
 outfile=open("CfloridaCounts.txt","w")
 
 import re
-texasfind=re.search("[cC][fF](0-7)?\.[Aa](2)?\.d{3}",vcffile)
+texasfind=re.compile("[cC][fF](0-9)?\.[Aa](0-9)?\.(0-9){3}")
 
 #Loop through open file
-for Line in vcffile:
+for line in vcffile:
     #Remove end of line character
-    Line = Line.strip()
-    #Operate only on lines that do not include >, skips header lines
-    if ">" not in Line:
-        #Find the ORF in each sequence line
-        texasfind=re.search(r"[cC][fF](0-7)?\.[Aa](2)?\.d{3}",Line)
-        #Print the ORF to standard out, note that group(0) is the full match
-        print(texasfind.group(0))
-    elif : #how can you tell if this is the line with the column headings?
+    line = line.strip()
+    if "##" in line:
+        outfile.write(line + "\n")
+    elif "#" in line: #how can you tell if this is the line with the column headings?
         #standardize (replace) sample names with TX and FL regexes
-        re.sub()
         #write new version of line to file
-        outfile.write(string + "\n")
-    else: #now you're in the data
-        #replace full SNP info with allele counts only
-        #replace missing data with NA
-        #write new version of line to new file
-
 vcffile.close()
+outfile.close()
 
 # need to add the loop after checking that the regex code above works for the file
 #other possible REGEX pattern 
-import re
-texas=re.search(r"[cC][fF]([0-9]{2})?\.[Aa]([0-9])?\.[X]{3}",vcffile)
+#import re
+#texas=re.search(r"[cC][fF]([0-9]{2})?\.[Aa]([0-9])?\.[X]{3}",vcffile)
 
 
