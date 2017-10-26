5163b1f30f6cf675bd393b4e82d76f02721ccbfe
kkilgoreND <31992787+kkilgoreND@users.noreply.github.com>
2017-10-26 15:24:23 -0400

    possible final commit...

--- Exercise08_Q1.py
+++ Exercise08_Q1.py
@@ -5,16 +5,34 @@ vcf = open("Cflorida.vcf","r")
 outfile = open("CfloridaCounts.txt","w")
 
 #assign regex to variable(might need 3: one for texas, one for florida and one for SNPs)
-TX_reg=re.compile(input regex) #tx and fl should be the same (format re.compile(r''))
-FL_reg=re.compile(input regex)
-SNP_reg=re.compile(r'^.+?/.+?:(.+?,.+?):.+?,.+?,.+?$') #not 100% sure it's correct
-#re.sub(r"[Cc][Ff][0-9]+\.[Aa][0-9]{1}\.[0-9]{3}","Cf.Sfa.[0-9]{3}",test)>test1 #in case this is helpful for searches
+TX_reg=r"[Cc].{1,3}\.[a|A][0-9]?\.([0-9]{3})"
+FL_reg=r"[Cc].{1,3}\.[g|G].{1,2}\.([0-9]{3})"
+SNP_reg=r"[0|1]\/[0|1]\:([0-9]{1,3}\,[0-9]{1,3})\:[0-9]{1,3}\:[0-9]{1,3}\:[0-9]{1,3}\,[0-9]{1,3}\,[0-9]{1,3}"
+Fin_reg=r"\.\/\.\:\.\:\.\:\.\:\."
+#I think these should work... not 100%
+
+#re.sub(r"[Cc][Ff][0-9]+\.[Aa][0-9]{1}\.[0-9]{3}","Cf.Sfa.[0-9]{3}",test)>test1 
+#original regex idea to do it all at once....
+
 #loop(following similar format to pseudo code)
-for line in vcf:
-  line=line.strip()
-  #split lines int columns separated by whitespace
-  col=line.split()
-  if 
-  elif
-  else
-  outfile.write( + "\n")
+for Line in vcffile:
+    Line = Line.strip() #strip end of line
+    if '##' in Line: 
+        outfile.write(Line + "\n") 
+        # write header(unchanged) line to file
+    elif '#' in Line: 
+        #replace sample names with TX and FL regexes
+        Line_2=re.sub(TX_reg, "Cf.Sfa.\g<1>", Line)
+        Line_3=re.sub(FL_reg, "Cf.Gai.\g<1>", Line_2)
+        #write new line version to file
+        outfile.write(Line_3 + "\n") 
+    else: #now you're in the data
+        Line_2=re.sub(SNP_reg, "\g<1>", Line)
+        #replaces full SNP info with allele counts
+        #replace missing data with NA
+        Line_3=re.sub(Fin_reg, "NA", Line_2)
+        #write new line version to file
+        outfile.write(Line_3 + "\n")
+#Close files
+outfile.close() 
+vcf.close()
