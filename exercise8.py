2e27b68a5b146188182f04aa85bbce5fdcd1c6da
Katherine <bookaholic214@gmail.com>
2017-10-25 20:50:34 -0400

    Added if statement to forloop

--- exercise8.py
+++ exercise8.py
@@ -5,16 +5,21 @@
 
 import re
 vcf = open("Cflorida.vcf","r")
-
+outfile = open("CfloridaCounts.txt","w")
 search_tex =r'(CF.A.|CF.A2.|CF07.A.|cf.a.)([0-9]{3})'
 replace_tex =r'Cf.Sfa.\2'
 
 for line in vcf:
     line = line.strip()
-    if "#" in line:
+    if "##" in line:
+		outfile.write(line + "\n")	
+	elif "#" in line:
         texas = re.sub(search_tex, replace_tex, line)
+        outfile.append(line + "\n")
         print texas
-
+	else:
+        #select lines with "Contig", find 0/0:, keep 1 digit after that, get rid of everything else
+        outfile.append(line + "\n")
 vcf.close()
 
 
