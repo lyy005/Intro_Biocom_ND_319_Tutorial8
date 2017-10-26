772d6eb538ca3802d591a5bb67603470466b7566
Michelle Corley <mcorley1@nd.edu>
2017-10-26 15:32:27 -0400

    finished second objective of part 1

--- part1script.py
+++ part1script.py
@@ -1,5 +1,5 @@
 #Exercise 8, Question 1
-#10/13/17, MRC
+#10/13/17
 import re
 
 #Open files to read and write
@@ -9,6 +9,8 @@ outfile = open("CfloridaCounts.txt","w")
 #regex variables
 TXsamples = "([Cc][Ff](\d{2})?\.[Aa]\d?)"
 FLsamples = "([Cc][Ff]\.[Gg](2|AI|ai))"
+allelect = r"\d/\d:(\d,\d):\d:\d+:\d+,\d+,\d+"
+missing = "\./\.:\.:\.:\.:\."
 
 for Line in vcffile:
     Line=Line.strip()
@@ -20,10 +22,12 @@ for Line in vcffile:
         TXreplaced = re.sub(TXsamples,"Cf.Sfa",Line)
         FLreplaced = re.sub(FLsamples,"Cf.Gai",TXreplaced) #standardize (replace) sample names with TX and FL regexes
         outfile.write(FLreplaced + "\n") #write new version of line to file
-    #else: #now you're in the data
-        #replace full SNP info with allele counts only
-        #replace missing data with NA
-        #write new version of line to new file
+    else: #now you're in the data
+        selAllele = re.sub(allelect, r"\1", Line) #replace full SNP info with allele counts only
+        #print(selAllele)
+        missingData = re.sub(missing, "N/A", selAllele) #replace missing data with NA
+        #print(missingData)
+        outfile.write(missingData +"\n") #write new version of line to new file
         
 vcffile.close()
 outfile.close()
