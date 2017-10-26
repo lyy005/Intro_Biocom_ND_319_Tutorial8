7efc62793f55a9e2c6f18905a04879f5e4236162
omegadan01 <dbruzzes@nd.edi>
2017-10-26 19:19:33 -0400

    now puuuushin

--- exercise8.py
+++ exercise8.py
@@ -2,33 +2,37 @@
 
 
 #####Question 1
-
+import sys
 import re
 vcf = open("Cflorida.vcf","r")
 outfile = open("CfloridaCounts.txt","w")
 search_tex = r'(CF.A.|CF.A2.|CF07.A.|cf.a.)([0-9]{3})'
 replace_tex = r'Cf.Sfa.\2'
 search_fl = r'(CF.G2.|CF.GAI.|cf.gai.)([0-9]{3})'
-replace_fl = r'Cf.Gai.\4'
+replace_fl = r'Cf.Gai.\2'
+
+search_allel = r'[0-1]/[0-1]:(\d{1},\d{1})'
+replace_allel = r'\2'
+
+
+match = r'.\'
+fix = r'NA'
+
+
 
 for line in vcf:
     line = line.strip()
     if "##" in line:
-        outfile.write(line + "\n")
+        outfile.write (line + "\n")
     elif "#" in line:
-        texas = re.sub(search_tex, replace_tex, line)
-        florida= re.sub(search_fl, replace_fl, line)
-        outfile.append(line + "\n")
+        line = re.sub(search_tex, replace_tex, line)
+        line = re.sub(search_fl, replace_fl, line)
+        outfile.write(line + "\n")
     else:
-        #select lines with "Contig", find 0/0:, keep 1 digit after that, get rid of everything else
-        if:
-            match = re.search(([0-1]/[0-1]:)(\d{1},\d{1}))
-            m.group(2)
-        else:
-            match = re.search(([0-1]/[0-1]:)(\.,{1}))
-            m.group(2)
-            #replace with NA
-        outfile.append(line + "\n")
+        line = re.sub(search_allel, replace_allel)
+        outfile.write(line + "\n")
+
+outfile.close()
 vcf.close()
 
 
